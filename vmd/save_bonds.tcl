44e18473f1b290693d53ce6e2b575d1fd3e311ac
peanut231 <47799290+peanut231@users.noreply.github.com>
2020-09-14 10:54:11 -0700

    fix files

--- vmd/save_bonds.tcl
+++ vmd/save_bonds.tcl
@@ -1,30 +1,24 @@
 
-set f [open "dihedrals.dat" w]
+set f [open "bonds.dat" w]
 
-foreach dihedral [label list Dihedrals] {
-  # Dihedral atom indices
-  set i1 [lindex [split [lindex $dihedral 0]] 1]
-  set i2 [lindex [split [lindex $dihedral 1]] 1]
-  set i3 [lindex [split [lindex $dihedral 2]] 1]
-  set i4 [lindex [split [lindex $dihedral 3]] 1]
+foreach bond [label list Bonds] {
+  # Bond atom indices
+  set i1 [lindex [split [lindex $bond 0]] 1]
+  set i2 [lindex [split [lindex $bond 1]] 1]
   
-  # Dihedral atom names
+  # Bond atom names
   set a1 [[atomselect top "index $i1"] get name]
   set a2 [[atomselect top "index $i2"] get name]
-  set a3 [[atomselect top "index $i3"] get name]
-  set a4 [[atomselect top "index $i4"] get name]
   
-  # Dihedral atom resid
+  # Bond atom resid
   set r1 [[atomselect top "index $i1"] get resid]
   set r2 [[atomselect top "index $i2"] get resid]
-  set r3 [[atomselect top "index $i3"] get resid]
-  set r4 [[atomselect top "index $i4"] get resid]
   
-  # Dihedral angle
-  set dih [lindex $dihedral 4]
+  # Bond
+  set dist [lindex $bond 2]
   
   # Print
-  puts $f ":${r1}@${a1} :${r2}@${a2} :${r3}@${a3} :${r4}@${a4}  $dih"
+  puts $f ":${r1}@${a1} :${r2}@${a2} $dist"
 }
 
 close $f
